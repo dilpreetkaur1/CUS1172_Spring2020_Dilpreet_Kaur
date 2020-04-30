af7901bebd80a12f8a0ed2995980c8357955ff2c
dilpreetkaur1 <54918371+dilpreetkaur1@users.noreply.github.com>
2020-04-30 17:29:52 -0400

    quiz id

--- Milestone 2/quiz1.js
+++ Milestone 2/quiz1.js
@@ -14,4 +14,18 @@ router.get('/api/quiz/list', function(req, res){
     //  quiz_list= [{"quiz1":quiz1.quiz1_id,"quiz2": quiz1.quiz2_id}];
      res.json({"message":[{"quiz1":quiz1.quiz1_id,"quiz2": quiz1.quiz2_id}]})
 });
+// route to get quiz agains quiz id
+router.get('/api/quiz/:id', function(req, res){
+    // res.send('quizes.');
 
+    const id=req.params.id
+    var data;
+    console.log("id is here ", id)
+    if(id === "quiz1"){
+         data=quiz1.quiz1
+    }else{
+        data=quiz1.quiz2
+
+    }
+res.json({"message": data })
+});

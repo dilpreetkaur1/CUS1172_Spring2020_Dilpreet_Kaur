559544ad3479b47c0f42ba7127a45c9e48e0b416
dilpreetkaur1 <54918371+dilpreetkaur1@users.noreply.github.com>
2020-05-01 16:00:26 -0400

    route 1 implemented for quiz 1

---
+++ Milestone 2/index_milestone2.js
@@ -0,0 +1,28 @@
+var right_answers = 0;
+var quiz1_current_actual_chkbox_ans;
+var quiz1_array_count = 0;
+var quiz1_res;
+var question_count = 1;
+var quiz1_current_user_ans = "";
+var quiz1_current_actual_ans = "";
+var quiz1_current_explanation = ""
+
+var quiz_choice = ""
+var q_id = 1;  
+var q_meta=1;
+var choices;
+$.ajax({
+    url: "http://localhost:5000/quiz1/api/quiz/list",
+
+    method: "get",
+    contentType: "json/application",
+    dataType: 'json',
+    success: function (response) {
+        console.log("api response", response)
+        // quiz1_res = response;
+        choices = response.message
+    },
+    error: function (err) {
+        console.log("errors : ", err)
+    }
+})

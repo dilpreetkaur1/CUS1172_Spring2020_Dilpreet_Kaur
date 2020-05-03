90e77ec64b8e429cd947f0d10145c2d666b59bc3
dilpreetkaur1 <54918371+dilpreetkaur1@users.noreply.github.com>
2020-05-03 15:48:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ var quiz1 = require('./quiz1');
 var cors = require('cors')
 app.use(cors()) 
 app.use('/quiz1', quiz1);
-const http = require('http');
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
-});
 const PORT = process.env.port || 5000;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
